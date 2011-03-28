753e9dd6f15607200380ef07273525a1dfab3580
Marco Pfiffner <m1pfiffn@hsr.ch>
2011-03-28 16:33:32 +0200

    Server update

--- GymtasticApp/src/network/RMIServer.java
+++ GymtasticApp/src/network/RMIServer.java
@@ -1,12 +1,33 @@
 package network;
 
-public class RMIServer {
+import java.rmi.RemoteException;
+import java.util.Vector;
+
+public class RMIServer implements RMIServerInterface {
 
     /**
      * @param args
      */
+    Vector<Object> clients = new Vector<Object>();
+    
+    
+    
+    public RMIServer() {
+	super();
+    }
+
+    @Override
+    public void addClient(RMIClientInterface client) throws RemoteException {
+	
+    }
+
+    @Override
+    public void removeClient() throws RemoteException {
+	
+    }
+    
+    
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
 
     }
 
