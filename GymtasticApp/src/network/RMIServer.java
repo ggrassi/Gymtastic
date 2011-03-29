2715f55e04f7afd2141b0d4b5da1723c5f4f4e39
Mathias Fasser <mathias@Mathias-MBP.local>
2011-03-29 17:06:46 +0200

    network update

--- GymtasticApp/src/network/RMIServer.java
+++ GymtasticApp/src/network/RMIServer.java
@@ -10,52 +10,53 @@ import domain.Dummy;
 
 public class RMIServer implements RMIServerInterface {
 
-    /**
-     * @param args
-     */
-    Vector<RMIClientInterface> clients = new Vector<RMIClientInterface>();
+	/**
+	 * @param args
+	 */
+	Vector<RMIClientInterface> clients = new Vector<RMIClientInterface>();
 
-    public RMIServer() {
-	super();
-    }
-
-    @Override
-    public void addClient(RMIClientInterface client) throws RemoteException {
-	clients.add(client);
-	System.out.println("Client added");
-
-    }
-
-    @Override
-    public void removeClient(RMIClientInterface client) throws RemoteException {
-	clients.remove(client);
-	System.out.println("Client removed!");
-
-    }
-    
-    private void updateClients() throws RemoteException{
-	for(RMIClientInterface client: clients){
-	    client.updateClient(new Dummy("test"));
-	}
-    }
-
-    public static void main(String[] args) throws RemoteException {
-	System.out.println("[GymTastic-Server]");
-	RMIServer server = new RMIServer();
-	System.out.println("[GymTastic] generate proxy");
-	RMIServerInterface stub = (RMIServerInterface) UnicastRemoteObject.exportObject(server, 0);
-	System.out.println("[GymTastic] registry()");
-	Registry registry = LocateRegistry.createRegistry(1099);
-	registry.rebind("Server", stub);
-	while(true){
-	    server.updateClients();
-	    try {
-		Thread.sleep(500);
-	    } catch (InterruptedException e) {
-		e.printStackTrace();
-	    }
+	public RMIServer() {
+		super();
 	}
 
-    }
+	@Override
+	public void addClient(RMIClientInterface client) throws RemoteException {
+		clients.add(client);
+		System.out.println("Client added");
+
+	}
+
+	@Override
+	public void removeClient(RMIClientInterface client) throws RemoteException {
+		clients.remove(client);
+		System.out.println("Client removed!");
+
+	}
+
+	private void updateClients() throws RemoteException {
+		for (RMIClientInterface client : clients) {
+			client.updateClient(new Dummy("test"));
+		}
+	}
+
+	public static void main(String[] args) throws RemoteException {
+		System.out.println("[GymTastic-Server]");
+		RMIServer server = new RMIServer();
+		System.out.println("[GymTastic] generate proxy");
+		RMIServerInterface stub = (RMIServerInterface) UnicastRemoteObject
+				.exportObject(server, 0);
+		System.out.println("[GymTastic] registry()");
+		Registry registry = LocateRegistry.createRegistry(1099);
+		registry.rebind("Server", stub);
+		while (true) {
+			server.updateClients();
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
