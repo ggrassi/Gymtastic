60beba466b7d681023bfd43c354da6dc3231a6ea
Jules Weder <julesweder@gmail.com>
2011-03-30 13:31:10 +0200

    Riegen aus Import generieren

--- GymtasticApp/src/domain/GymtasticApp.java
+++ GymtasticApp/src/domain/GymtasticApp.java
@@ -1,10 +1,14 @@
 package domain;
 
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import importer.ImportStartList;
 
 public class GymtasticApp {
+
+private static final int squadPositionImport = 0;
 
     /**
      * @param args
@@ -14,9 +18,33 @@ public class GymtasticApp {
 
 	/* Import the starter file */
 	ImportStartList startList = new ImportStartList("src/importer/Startliste_Bsp.txt");
+	System.out.println("******** Import *************");
 	startList.readImport();
 	startList.toString();
+	
+	/* generate Squads with importlist */
+	System.out.println("******** Squad Generator *************");
+	createSquad(startList);
+	
+	
+    }
 
+    private static void createSquad(ImportStartList startList) {
+	
+	//Set mit Riegen erstellen
+	Set<Integer> squadsList = new TreeSet<Integer>();
+	List<List<String>> importList = startList.getImportList();
+	for (List<String> line : importList) {
+	    squadsList.add(Integer.parseInt(line.get(squadPositionImport)));
+	}
+	
+	// Ausgabe aller Riegennummern
+	for (Integer squadNr : squadsList) {
+	    System.out.println(squadNr);
+	}
+	
+	// Anhand der Riegennummern die Riegen erstellen
+	
     }
 
 }
