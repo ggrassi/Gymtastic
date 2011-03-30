6c18c64fd7a2cdc1f2c4064b8fd9954704f00753
Jules Weder <julesweder@gmail.com>
2011-03-30 14:26:47 +0200

    createSquads fertig

--- GymtasticApp/src/domain/GymtasticApp.java
+++ GymtasticApp/src/domain/GymtasticApp.java
@@ -1,14 +1,16 @@
 package domain;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-
 import importer.ImportStartList;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class GymtasticApp {
 
-private static final int squadPositionImport = 0;
+    private static final int squadPositionImport = 0;
 
     /**
      * @param args
@@ -21,30 +23,35 @@ private static final int squadPositionImport = 0;
 	System.out.println("******** Import *************");
 	startList.readImport();
 	startList.toString();
-	
+
 	/* generate Squads with importlist */
 	System.out.println("******** Squad Generator *************");
-	createSquad(startList);
+	Map<Integer, Squad> squadMap = createSquads(startList);
 	
-	
+
     }
 
-    private static void createSquad(ImportStartList startList) {
-	
-	//Set mit Riegen erstellen
-	Set<Integer> squadsList = new TreeSet<Integer>();
+    private static Map<Integer, Squad> createSquads(ImportStartList startList) {
 	List<List<String>> importList = startList.getImportList();
-	for (List<String> line : importList) {
-	    squadsList.add(Integer.parseInt(line.get(squadPositionImport)));
-	}
-	
-	// Ausgabe aller Riegennummern
-	for (Integer squadNr : squadsList) {
-	    System.out.println(squadNr);
-	}
-	
-	// Anhand der Riegennummern die Riegen erstellen
-	
-    }
 
+	Set<Integer> squadsNrList = new TreeSet<Integer>();
+	for (List<String> line : importList) {
+	    squadsNrList.add(Integer.parseInt(line.get(squadPositionImport)));
+	}
+
+	Map<Integer, Squad> squadMap = new TreeMap<Integer, Squad>();
+	for (Integer squadNr : squadsNrList) {
+	    squadMap.put(squadNr, new Squad(squadNr));
+	}
+
+	for (List<String> line : importList) {
+	    squadMap.get(Integer.parseInt(line.get(0))).addAthlete(
+		    new Athlet(Integer.parseInt(line.get(0)), Integer.parseInt(line.get(1)), line.get(2), line.get(3),
+			    line.get(4), line.get(5), Integer.parseInt(line.get(6)), new Verein(line.get(7), line
+				    .get(8))));
+	}
+	
+	return squadMap;
+
+    }
 }
