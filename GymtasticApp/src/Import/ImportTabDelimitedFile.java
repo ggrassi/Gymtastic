b229c1a03d0acbb2711eaa94c748b7cbace0377b
Jules Weder <julesweder@gmail.com>
2011-03-30 09:21:18 +0200

    delete old code csv import

--- GymtasticApp/src/Import/ImportTabDelimitedFile.java
+++ GymtasticApp/src/Import/ImportTabDelimitedFile.java
@@ -1,7 +1,6 @@
 package Import;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,7 +20,7 @@ public class ImportTabDelimitedFile {
     private String address;
     private String birth;
     private String vereinsname;
-    private String Vereinsort;
+    private String vereinsort;
 
     public ImportTabDelimitedFile(String[] persInfo) {
 	startNr = persInfo[0];
@@ -32,18 +31,17 @@ public class ImportTabDelimitedFile {
 	address = persInfo[5];
 	birth = persInfo[6];
 	vereinsname = persInfo[7];
-	Vereinsort = persInfo[8];
+	vereinsort = persInfo[8];
     }
 
     public static void main(String[] args) {
 	BufferedReader readbuffer = null;
 	try {
 	    readbuffer = new BufferedReader(new FileReader("src/Import/Startliste_Bsp.txt"));
-
-	    String strRead;
+	    String line;
 	    List<ImportTabDelimitedFile> importList = new ArrayList<ImportTabDelimitedFile>();
-	    while ((strRead = readbuffer.readLine()) != null) {
-		String splitArray[] = strRead.split("\t");
+	    while ((line = readbuffer.readLine()) != null) {
+		String splitArray[] = line.split("\t");
 		importList.add(new ImportTabDelimitedFile(splitArray));
 	    }
 	} catch (IOException e) {
@@ -55,78 +53,6 @@ public class ImportTabDelimitedFile {
 		e.printStackTrace();
 	    }
 	}
-    }
-
-    public void setStartNr(String startNr) {
-	this.startNr = startNr;
-    }
-
-    public String getStartNr() {
-	return startNr;
-    }
-
-    public void setSquad(String squad) {
-	this.squad = squad;
-    }
-
-    public String getSquad() {
-	return squad;
-    }
-
-    public void setProgramClass(String programClass) {
-	this.programClass = programClass;
-    }
-
-    public String getProgramClass() {
-	return programClass;
-    }
-
-    public void setFirstName(String firstName) {
-	this.firstName = firstName;
-    }
-
-    public String getFirstName() {
-	return firstName;
-    }
-
-    public void setSurName(String surName) {
-	this.surName = surName;
-    }
-
-    public String getSurName() {
-	return surName;
-    }
-
-    public void setAddress(String address) {
-	this.address = address;
-    }
-
-    public String getAddress() {
-	return address;
-    }
-
-    public void setBirth(String birth) {
-	this.birth = birth;
-    }
-
-    public String getBirth() {
-	return birth;
-    }
-
-    public void setVereinsname(String vereinsname) {
-	this.vereinsname = vereinsname;
-    }
-
-    public String getVereinsname() {
-	return vereinsname;
-    }
-
-    public void setVereinsort(String vereinsort) {
-	Vereinsort = vereinsort;
-    }
-
-    public String getVereinsort() {
-	return Vereinsort;
     }
 
 }
