690eafa451bb68c7bad26badff8ecba5aa02cada
ggrassi netbeans <ggrassi@hsr.ch>
2011-03-30 11:18:35 +0200

    squad und athlet update

--- GymtasticApp/src/domain/Squad.java
+++ GymtasticApp/src/domain/Squad.java
@@ -1,23 +1,38 @@
 package domain;
 
 import java.io.Serializable;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.UUID;
 
 public class Squad implements Serializable {
-	private static int id;
-	private int squadId = 0;
-	private Set<Athlet> athlets = null;
 
-	public Squad() {
-		squadId = id++;
-		athlets = new HashSet<Athlet>();
+    private static final long serialVersionUID = -4834032781011953418L;
+    private Set<Athlet> athlets;
+    private final int squadId;
+
+    public Squad(int squadId) {
+	this.squadId = squadId;
+	athlets = new TreeSet<Athlet>();
+    }
+
+    public int getId() {
+	return squadId;
+    }
+
+    public void addAthlete(Athlet athlet) {
+	if (athlet != null && athlet.getSquadID() == squadId) {
+	    athlets.add(athlet);
 	}
+    }
+    
+    public Athlet getAthlete(){
+	return null;
+    }
 
-	public void addAthlete(Athlet athlet) {
-		if (athlet != null) {
-			athlets.add(athlet);
-		}
-	}
-
+    @Override
+    public String toString() {
+	return "Squad [squadId=" + squadId + "]";
+    }
+    
 }
