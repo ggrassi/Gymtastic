3dc967f94ce125079a00359878de55621f933a28
Marco Pfiffner <m1pfiffn@hsr.ch>
2011-03-30 15:35:00 +0200

    network/ui update

--- GymtasticApp/src/network/RMIClient.java
+++ GymtasticApp/src/network/RMIClient.java
@@ -1,25 +1,43 @@
 package network;
 
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+
+import views.ClientPrototype;
 import network.RMIServerInterface;
 
 import domain.Dummy;
 
 public class RMIClient implements RMIClientInterface {
 
-//	private static final String HOST = "152.96.233.102";
-	private static final String HOST = "localhost";
-//	private static final String HOST = "192.168.0.100";
+	// private static final String HOST = "152.96.233.102";
+	// private static final String HOST = "192.168.0.100";
+	private String serverIP = "localhost";
 	private RMIServerInterface rmiServerInterface;
 	private Dummy dummy;
 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		System.out.println("Client gestartet");
+		RMIClient client = new RMIClient();
+		ClientPrototype.newClientPrototypeFrame(client);
+
+	}
+
 	public RMIClient() throws Exception {
 
+	}
+
+	public void connect() throws RemoteException, NotBoundException,
+			AccessException {
 		System.out.println("generate registry");
-		Registry registry = LocateRegistry.getRegistry(HOST);
+		Registry registry = LocateRegistry.getRegistry(getServerIP());
 
 		System.out.println("registry lookup");
 		rmiServerInterface = (RMIServerInterface) registry.lookup("Server");
@@ -29,11 +47,12 @@ public class RMIClient implements RMIClientInterface {
 				.exportObject(this, 0);
 
 		System.out.println("Aufruf von addClient auf ServerStub");
-		rmiServerInterface.addClient(stub, RMIServer.POMMEL_HORSE);
+		rmiServerInterface.addClient(stub, RMIServer.FLOOR_EXCERICE);
 		System.out.println("Aufruf von addClient auf ServerStub ausgeführt");
-		
-		
-		
+	}
+
+	public void disconnect() throws RemoteException {
+		rmiServerInterface.removeClient(this);
 
 	}
 
@@ -42,26 +61,28 @@ public class RMIClient implements RMIClientInterface {
 		rmiServerInterface.uploadSquadToServer(this.dummy);
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-		System.out.println("Client gestartet");
-		@SuppressWarnings("unused")
-		RMIClient client = new RMIClient();
-
-	}
-
 	public void uploadSquadToClient(Dummy dummy) throws RemoteException {
 		this.dummy = dummy;
 		System.out.println("Dummy mit Name: " + dummy.getName()
 				+ "ist bei Client eingetroffen.");
-		
+
 		this.dummy.setName("SuuuperDuuuuperDummy");
 		System.out.println("Neuer Dummy name: " + this.dummy.getName());
-		
+
 		serverUpdate();
 
+	}
+
+	public void setServerIP(String serverIP) {
+		this.serverIP = serverIP;
+	}
+
+	public String getServerIP() {
+		return serverIP;
+	}
+
+	public Dummy getDummy() {
+		return dummy;
 	}
 
 }
--- GymtasticApp/src/views/ClientPrototype.java
+++ GymtasticApp/src/views/ClientPrototype.java
@@ -1,48 +1,60 @@
 package views;
 
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import java.awt.GridBagLayout;
-import javax.swing.JPanel;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.border.TitledBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 import java.awt.FlowLayout;
-import javax.swing.JButton;
-import java.awt.BorderLayout;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+import java.util.Observable;
+import java.util.Observer;
 
-public class ClientPrototype {
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+import network.RMIClient;
+import domain.Dummy;
+
+public class ClientPrototype implements Observer {
 
 	private JFrame frmGymtasticClient;
 	private JTextField txtServerIP;
 	private JTextField textField;
+	private final RMIClient client;
+	private Dummy dummy;
+	final JButton btnDisconnect = new JButton("Disconnect");
+	final JButton btnConnect = new JButton("Connect");
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public ClientPrototype() {
+	public static void newClientPrototypeFrame(final RMIClient client) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ClientPrototype window = new ClientPrototype();
+					ClientPrototype window = new ClientPrototype(client);
 					window.frmGymtasticClient.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+
+	public ClientPrototype(RMIClient client) {
+		this.client = client;
 		initialize();
 	}
 
@@ -56,14 +68,15 @@ public class ClientPrototype {
 		frmGymtasticClient.setBounds(100, 100, 306, 281);
 		frmGymtasticClient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
 		frmGymtasticClient.getContentPane().setLayout(gridBagLayout);
-		
+
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Connection", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBorder(new TitledBorder(null, "Connection",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		GridBagConstraints gbc_panel = new GridBagConstraints();
 		gbc_panel.insets = new Insets(0, 0, 5, 0);
 		gbc_panel.fill = GridBagConstraints.BOTH;
@@ -71,12 +84,12 @@ public class ClientPrototype {
 		gbc_panel.gridy = 0;
 		frmGymtasticClient.getContentPane().add(panel, gbc_panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0};
-		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[] { 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0, 0 };
+		gbl_panel.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
-		
+
 		JPanel panel_2 = new JPanel();
 		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
 		gbc_panel_2.insets = new Insets(0, 0, 5, 0);
@@ -85,12 +98,12 @@ public class ClientPrototype {
 		gbc_panel_2.gridy = 0;
 		panel.add(panel_2, gbc_panel_2);
 		GridBagLayout gbl_panel_2 = new GridBagLayout();
-		gbl_panel_2.columnWidths = new int[]{0, 0, 0};
-		gbl_panel_2.rowHeights = new int[]{0, 0};
-		gbl_panel_2.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_2.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel_2.columnWidths = new int[] { 0, 0, 0 };
+		gbl_panel_2.rowHeights = new int[] { 0, 0 };
+		gbl_panel_2.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+		gbl_panel_2.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
 		panel_2.setLayout(gbl_panel_2);
-		
+
 		JLabel lblServerIP = new JLabel("Server-IP:");
 		GridBagConstraints gbc_lblServerIP = new GridBagConstraints();
 		gbc_lblServerIP.insets = new Insets(0, 0, 0, 5);
@@ -98,8 +111,8 @@ public class ClientPrototype {
 		gbc_lblServerIP.gridx = 0;
 		gbc_lblServerIP.gridy = 0;
 		panel_2.add(lblServerIP, gbc_lblServerIP);
-		
-		txtServerIP = new JTextField();
+
+		txtServerIP = new JTextField(client.getServerIP());
 		txtServerIP.setToolTipText("example: 192.168.1.100");
 		GridBagConstraints gbc_txtServerIP = new GridBagConstraints();
 		gbc_txtServerIP.fill = GridBagConstraints.HORIZONTAL;
@@ -107,7 +120,7 @@ public class ClientPrototype {
 		gbc_txtServerIP.gridy = 0;
 		panel_2.add(txtServerIP, gbc_txtServerIP);
 		txtServerIP.setColumns(10);
-		
+
 		JPanel panel_3 = new JPanel();
 		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
 		gbc_panel_3.anchor = GridBagConstraints.EAST;
@@ -116,28 +129,49 @@ public class ClientPrototype {
 		gbc_panel_3.gridy = 1;
 		panel.add(panel_3, gbc_panel_3);
 		panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		JButton btnDisconnect = new JButton("Disconnect");
+		btnDisconnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				enableConnectionPanel();
+				try {
+					client.disconnect();
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
 		btnDisconnect.setEnabled(false);
 		panel_3.add(btnDisconnect);
-		
-		JButton btnConnect = new JButton("Connect");
+
+		btnConnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					disableConnectionPanel();
+					client.connect();
+
+				} catch (Exception e) {
+
+				}
+			}
+
+		});
 		panel_3.add(btnConnect);
-		
+
 		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(null, "Information", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBorder(new TitledBorder(null, "Information",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
 		gbc_panel_1.fill = GridBagConstraints.BOTH;
 		gbc_panel_1.gridx = 0;
 		gbc_panel_1.gridy = 1;
 		frmGymtasticClient.getContentPane().add(panel_1, gbc_panel_1);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{0, 0};
-		gbl_panel_1.rowHeights = new int[]{0, 0, 0};
-		gbl_panel_1.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.columnWidths = new int[] { 0, 0 };
+		gbl_panel_1.rowHeights = new int[] { 0, 0, 0 };
+		gbl_panel_1.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panel_1.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
 		panel_1.setLayout(gbl_panel_1);
-		
+
 		JPanel panel_5 = new JPanel();
 		GridBagConstraints gbc_panel_5 = new GridBagConstraints();
 		gbc_panel_5.insets = new Insets(0, 0, 5, 0);
@@ -146,12 +180,12 @@ public class ClientPrototype {
 		gbc_panel_5.gridy = 0;
 		panel_1.add(panel_5, gbc_panel_5);
 		GridBagLayout gbl_panel_5 = new GridBagLayout();
-		gbl_panel_5.columnWidths = new int[]{0, 0, 0};
-		gbl_panel_5.rowHeights = new int[]{0, 0, 0, 0};
-		gbl_panel_5.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_5.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_5.columnWidths = new int[] { 0, 0, 0 };
+		gbl_panel_5.rowHeights = new int[] { 0, 0, 0, 0 };
+		gbl_panel_5.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+		gbl_panel_5.rowWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		panel_5.setLayout(gbl_panel_5);
-		
+
 		JLabel lblName = new JLabel("Name:");
 		GridBagConstraints gbc_lblName = new GridBagConstraints();
 		gbc_lblName.anchor = GridBagConstraints.EAST;
@@ -159,7 +193,7 @@ public class ClientPrototype {
 		gbc_lblName.gridx = 0;
 		gbc_lblName.gridy = 0;
 		panel_5.add(lblName, gbc_lblName);
-		
+
 		textField = new JTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
 		gbc_textField.insets = new Insets(0, 0, 5, 0);
@@ -168,7 +202,7 @@ public class ClientPrototype {
 		gbc_textField.gridy = 0;
 		panel_5.add(textField, gbc_textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblEnote = new JLabel("E-Note:");
 		GridBagConstraints gbc_lblEnote = new GridBagConstraints();
 		gbc_lblEnote.anchor = GridBagConstraints.EAST;
@@ -176,16 +210,18 @@ public class ClientPrototype {
 		gbc_lblEnote.gridx = 0;
 		gbc_lblEnote.gridy = 1;
 		panel_5.add(lblEnote, gbc_lblEnote);
-		
+
 		JComboBox cBeNote = new JComboBox();
-		cBeNote.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		DefaultComboBoxModel cBModel = new DefaultComboBoxModel(new String[] { "0", "1", "2",
+				"3", "4", "5", "6", "7", "8", "9" });
+		cBeNote.setModel(cBModel);
 		GridBagConstraints gbc_cBeNote = new GridBagConstraints();
 		gbc_cBeNote.fill = GridBagConstraints.HORIZONTAL;
 		gbc_cBeNote.insets = new Insets(0, 0, 5, 0);
 		gbc_cBeNote.gridx = 1;
 		gbc_cBeNote.gridy = 1;
 		panel_5.add(cBeNote, gbc_cBeNote);
-		
+
 		JLabel lblDnote = new JLabel("D-Note:");
 		GridBagConstraints gbc_lblDnote = new GridBagConstraints();
 		gbc_lblDnote.anchor = GridBagConstraints.EAST;
@@ -193,15 +229,15 @@ public class ClientPrototype {
 		gbc_lblDnote.gridx = 0;
 		gbc_lblDnote.gridy = 2;
 		panel_5.add(lblDnote, gbc_lblDnote);
-		
+
 		JComboBox cBdNote = new JComboBox();
-		cBdNote.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		cBdNote.setModel(cBModel);
 		GridBagConstraints gbc_cBdNote = new GridBagConstraints();
 		gbc_cBdNote.fill = GridBagConstraints.HORIZONTAL;
 		gbc_cBdNote.gridx = 1;
 		gbc_cBdNote.gridy = 2;
 		panel_5.add(cBdNote, gbc_cBdNote);
-		
+
 		JPanel panel_4 = new JPanel();
 		GridBagConstraints gbc_panel_4 = new GridBagConstraints();
 		gbc_panel_4.anchor = GridBagConstraints.EAST;
@@ -209,10 +245,26 @@ public class ClientPrototype {
 		gbc_panel_4.gridx = 0;
 		gbc_panel_4.gridy = 1;
 		panel_1.add(panel_4, gbc_panel_4);
-		
+
 		JButton btnUpdate = new JButton("Update");
 		btnUpdate.setToolTipText("Transmitts Updates to Server");
 		panel_4.add(btnUpdate);
+	}
+
+	private void disableConnectionPanel() {
+		btnConnect.setEnabled(false);
+		btnDisconnect.setEnabled(true);
+		txtServerIP.setEnabled(false);
+	}
+	private void enableConnectionPanel() {
+		btnConnect.setEnabled(true);
+		btnDisconnect.setEnabled(false);
+		txtServerIP.setEnabled(true);
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+
 	}
 
 }
