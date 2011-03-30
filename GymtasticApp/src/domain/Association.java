d1c2adf07ff0bdfdf2dd7c08764d092e9e679b90
Jules Weder <julesweder@gmail.com>
2011-03-30 15:08:42 +0200

    association

---
+++ GymtasticApp/src/domain/Association.java
@@ -0,0 +1,67 @@
+package domain;
+
+public class Association {
+
+	private String name;
+	private String location;
+
+	public Association(String name, String location) {
+		super();
+		this.name = name;
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Verein [name=" + name + ", location=" + location + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((location == null) ? 0 : location.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Association other = (Association) obj;
+		if (location == null) {
+			if (other.location != null)
+				return false;
+		} else if (!location.equals(other.location))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
