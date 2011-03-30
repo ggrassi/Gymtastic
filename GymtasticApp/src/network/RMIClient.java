36a88207264bd82c32ebbdbcf77a0e37ac62e980
Mathias Fasser <mathias@Mathias-MBP.local>
2011-03-30 11:34:29 +0200

    network update

--- GymtasticApp/src/network/RMIClient.java
+++ GymtasticApp/src/network/RMIClient.java
@@ -10,21 +10,25 @@ import domain.Dummy;
 
 public class RMIClient implements RMIClientInterface {
 
-	private static final String HOST = "localhost";
+//	private static final String HOST = "152.96.233.102";
+	private static final String HOST = "192.168.1.5";
 	private RMIServerInterface rmiServerInterface;
 	private Dummy dummy;
 
 	public RMIClient() throws Exception {
 
+		System.out.println("generate registry");
 		Registry registry = LocateRegistry.getRegistry(HOST);
 
+		System.out.println("registry lookup");
 		rmiServerInterface = (RMIServerInterface) registry.lookup("Server");
 
+		System.out.println("stub object creation");
 		RMIClientInterface stub = (RMIClientInterface) UnicastRemoteObject
 				.exportObject(this, 0);
 
 		System.out.println("Aufruf von addClient auf ServerStub");
-		rmiServerInterface.addClient(stub);
+		rmiServerInterface.addClient(stub, RMIServer.FLOOR_EXCERICE);
 		System.out.println("Aufruf von addClient auf ServerStub ausgeführt");
 		
 		
@@ -34,7 +38,7 @@ public class RMIClient implements RMIClientInterface {
 
 	private void serverUpdate() throws RemoteException {
 		System.out.println("Bearbeiteter Dummy wird an Server übertragen.");
-		rmiServerInterface.updateServer(this.dummy);
+		rmiServerInterface.uploadSquadToServer(this.dummy);
 	}
 
 	/**
@@ -47,7 +51,7 @@ public class RMIClient implements RMIClientInterface {
 
 	}
 
-	public void updateClient(Dummy dummy) throws RemoteException {
+	public void uploadSquadToClient(Dummy dummy) throws RemoteException {
 		this.dummy = dummy;
 		System.out.println("Dummy mit Name: " + dummy.getName()
 				+ "ist bei Client eingetroffen.");
