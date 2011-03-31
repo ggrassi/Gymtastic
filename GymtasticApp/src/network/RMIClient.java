d73a1a5a44f53bf99d0c5588402203c693bb6c0b
Marco Pfiffner <m1pfiffn@hsr.ch>
2011-03-31 11:24:13 +0200

    client refactor

--- GymtasticApp/src/network/RMIClient.java
+++ GymtasticApp/src/network/RMIClient.java
@@ -13,8 +13,6 @@ import domain.Dummy;
 
 public class RMIClient extends Observable implements RMIClientInterface {
 
-	// private static final String HOST = "152.96.233.102";
-	// private static final String HOST = "192.168.0.100";
 	private String serverIP = "localhost";
 	private RMIServerInterface rmiServerInterface;
 
@@ -24,10 +22,8 @@ public class RMIClient extends Observable implements RMIClientInterface {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		System.out.println("Client gestartet");
 		RMIClient client = new RMIClient();
 		ClientPrototype.newClientPrototypeFrame(client);
-
 	}
 
 	public RMIClient() throws Exception {
@@ -36,18 +32,11 @@ public class RMIClient extends Observable implements RMIClientInterface {
 
 	public void connect() throws RemoteException, NotBoundException,
 			AccessException {
-		System.out.println("generate registry");
 		Registry registry = LocateRegistry.getRegistry(getServerIP());
-
-		System.out.println("registry lookup");
 		rmiServerInterface = (RMIServerInterface) registry.lookup("Server");
-
-		System.out.println("stub object creation");
 		RMIClientInterface stub = (RMIClientInterface) UnicastRemoteObject
 				.exportObject(this, 0);
-		System.out.println("Aufruf von addClient auf ServerStub");
 		rmiServerInterface.addClient(stub, RMIServer.FLOOR_EXCERICE);
-		System.out.println("Aufruf von addClient auf ServerStub ausgeführt");
 	}
 
 	public void disconnect() throws RemoteException {
@@ -56,25 +45,12 @@ public class RMIClient extends Observable implements RMIClientInterface {
 	}
 
 	public void updateServer() throws RemoteException {
-		System.out.println("Bearbeiteter Dummy wird an Server übertragen.");
 		rmiServerInterface.uploadSquadToServer(dummy);
 	}
 
 	public void uploadSquadToClient(Dummy dummy) throws RemoteException {
-		System.out.println("Received new Dummy " + dummy);
 		this.dummy = dummy;
 		updateObservers();
-
-		/*
-		 * System.out.println("Dummy mit Name: " + dummy.getName() +
-		 * "ist bei Client eingetroffen.");
-		 * 
-		 * this.dummy.setName("SuuuperDuuuuperDummy");
-		 * System.out.println("Neuer Dummy name: " + this.dummy.getName());
-		 * 
-		 * serverUpdate();
-		 */
-
 	}
 
 	private void updateObservers() {
