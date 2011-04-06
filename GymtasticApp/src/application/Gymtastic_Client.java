bfba665095dca135ada29ac90a1b87d43ed7585b
Mathias Fasser <mathias@Mathias-MBP.local>
2011-04-06 14:16:15 +0200

    hlhjklöhjökljfklödsjalökfjsadlkjfjsdklöfjadksfjlö2

--- GymtasticApp/src/application/Gymtastic_Client.java
+++ GymtasticApp/src/application/Gymtastic_Client.java
@@ -1,12 +1,16 @@
 package application;
 
+import views.ClientConnection;
+
 public class Gymtastic_Client {
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	
+	ClientConnection.newClientConnectionFrame();
+
 
     }
 
