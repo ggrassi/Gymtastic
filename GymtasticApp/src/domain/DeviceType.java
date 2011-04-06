cf2f2e43f51ee244af128108aa40a18610d66a37
Marco Pfiffner <m1pfiffn@hsr.ch>
2011-04-06 12:29:33 +0200

    Server DeviceType editable / DeviceType toString() method

--- GymtasticApp/src/domain/DeviceType.java
+++ GymtasticApp/src/domain/DeviceType.java
@@ -2,37 +2,58 @@ package domain;
 
 import java.io.Serializable;
 
-public enum DeviceType implements Serializable{
+public enum DeviceType implements Serializable {
 
-    FLOOR_EXCERCISE(0), POMMEL_HORSE(1), STILL_RINGS(2), VAULT(3), PARALLEL_BARS(4), HIGH_BAR(5);
+	FLOOR_EXCERCISE(0), POMMEL_HORSE(1), STILL_RINGS(2), VAULT(3), PARALLEL_BARS(
+			4), HIGH_BAR(5);
 
-    public int index;
+	public int index;
 
-    DeviceType(int index) {
-	this.index = index;
-    }
-
-    public int getIndex() {
-	return index;
-    }
-
-    public DeviceType getDevice(int index) {
-	switch (index) {
-	case 0:
-	    return FLOOR_EXCERCISE;
-	case 1:
-	    return POMMEL_HORSE;
-	case 2:
-	    return STILL_RINGS;
-	case 3:
-	    return VAULT;
-	case 4:
-	    return PARALLEL_BARS;
-	case 5:
-	    return HIGH_BAR;
-	default:
-	    return null;
+	DeviceType(int index) {
+		this.index = index;
 	}
-    }
 
+	public int getIndex() {
+		return index;
+	}
+
+	public DeviceType getDevice(int index) {
+		switch (index) {
+		case 0:
+			return FLOOR_EXCERCISE;
+		case 1:
+			return POMMEL_HORSE;
+		case 2:
+			return STILL_RINGS;
+		case 3:
+			return VAULT;
+		case 4:
+			return PARALLEL_BARS;
+		case 5:
+			return HIGH_BAR;
+		default:
+			return null;
+		}
+	}
+
+	public String toString() {
+		switch (index) {
+		case 0:
+			return "Boden";
+		case 1:
+			return "Pferd";
+		case 2:
+			return "Ringe";
+		case 3:
+			return "Sprung";
+		case 4:
+			return "Barren";
+		case 5:
+			return "Reck";
+		default:
+			return "";
+
+		}
+
+	}
 }
