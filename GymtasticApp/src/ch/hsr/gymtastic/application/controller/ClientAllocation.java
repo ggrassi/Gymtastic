8e972c857b42a91a57c6fa2e1bad49e61117cdc3
Jules Weder <julesweder@gmail.com>
2011-04-07 19:10:15 +0200

    clientallocation und round allocation in application.controller und evaluation.java wurde geloescht

---
+++ GymtasticApp/src/ch/hsr/gymtastic/application/controller/ClientAllocation.java
@@ -0,0 +1,56 @@
+package ch.hsr.gymtastic.application.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import ch.hsr.gymtastic.domain.DeviceType;
+import ch.hsr.gymtastic.technicalServices.network.ClientInformation;
+import ch.hsr.gymtastic.technicalServices.network.RMIClientInterface;
+
+
+public class ClientAllocation {
+
+	private Map<DeviceType, ClientInformation> alloc;
+
+	public ClientAllocation() {
+		alloc = new HashMap<DeviceType, ClientInformation>();
+	}
+
+	public void addAllocation(DeviceType deviceType, ClientInformation client) {
+		alloc.put(deviceType, client);
+	}
+
+	public void removeAllocation(DeviceType deviceType) {
+		alloc.remove(deviceType);
+	}
+
+	public boolean isEmpty() {
+		return alloc.isEmpty();
+	}
+
+	public int size() {
+		return alloc.size();
+	}
+
+	public Set<Entry<DeviceType, ClientInformation>> entrySet() {
+		return alloc.entrySet();
+	}
+
+	public void addAll(Vector<ClientInformation> clients) {
+		for (ClientInformation c : clients) {
+			System.out.println("Allocating Client: " + c.getHost() + " to "
+					+ c.getDeviceType());
+			alloc.put(c.getDeviceType(), c);
+
+		}
+
+	}
+	
+	public RMIClientInterface getClientStub( DeviceType deviceType){
+	    return alloc.get(deviceType).getStub();
+	}
+
+}
