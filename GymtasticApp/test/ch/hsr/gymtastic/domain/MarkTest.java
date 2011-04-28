13a09cb6f7f53029f0ae0b3592b309a9284ad617
Marco Pfiffner <m1pfiffn@hsr.ch>
2011-04-28 16:37:29 +0200

    testSumOfEndMarks und SumOfEndMarks

--- GymtasticApp/test/ch/hsr/gymtastic/domain/MarkTest.java
+++ GymtasticApp/test/ch/hsr/gymtastic/domain/MarkTest.java
@@ -9,46 +9,46 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MarkTest {
-    Mark mark;
-    Mark emptyMark;
+	Mark mark;
+	Mark emptyMark;
 
-    @Before
-    public void runBeforeEveryTest() {
-	mark = new Mark(4.5, 4.3, 3.4, 4.5, 0.2, 0.5);
-	emptyMark = new Mark(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-    }
+	@Before
+	public void runBeforeEveryTest() {
+		mark = new Mark(4.5, 4.3, 3.4, 4.5, 0.2, 0.5);
+		emptyMark = new Mark(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+	}
 
-    @After
-    public void runAfterEveryTest() {
-    }
+	@After
+	public void runAfterEveryTest() {
+	}
 
-    @Test
-    public final void testDMarksAndEMarks() {
+	@Test
+	public final void testDMarksAndEMarks() {
 
-	assertEquals(4.5, mark.getdMark());
-	assertEquals(4.3, mark.geteMarkOne());
-	assertEquals(3.4, mark.getEmarkTwo());
-	assertEquals(4.5, mark.geteMarkThree());
-    }
+		assertEquals(4.5, mark.getdMark());
+		assertEquals(4.3, mark.geteMarkOne());
+		assertEquals(3.4, mark.getEmarkTwo());
+		assertEquals(4.5, mark.geteMarkThree());
+	}
 
-    @Test
-    public final void testPenalty() {
-	assertEquals(0.2, mark.getPenalty());
-    }
+	@Test
+	public final void testPenalty() {
+		assertEquals(0.2, mark.getPenalty());
+	}
 
-    @Test
-    public final void testBonus() {
-	assertEquals(0.5, mark.getBonus());
-    }
+	@Test
+	public final void testBonus() {
+		assertEquals(0.5, mark.getBonus());
+	}
 
-    @Test
-    public void testGetFinalMark() {
-	assertEquals(8.866666666666667, mark.getFinalMark());
-    }
-    
-    @Test
-    public void testEmptyMark() {
-	assertEquals(0.0, emptyMark.getFinalMark());
-    }
+	@Test
+	public void testGetFinalMark() {
+		assertEquals(8.866666666666667, mark.getFinalMark());
+	}
+
+	@Test
+	public void testEmptyMark() {
+		assertEquals(0.0, emptyMark.getFinalMark());
+	}
 
 }
