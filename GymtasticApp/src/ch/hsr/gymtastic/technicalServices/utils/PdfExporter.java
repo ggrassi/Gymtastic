fca8cbc7e9d359e08832624caccf60fbc1fadc37
Mathias Fasser <mathias@Mathias-MBP.local>
2011-05-02 22:11:00 +0200

    pdf exporter

--- GymtasticApp/src/ch/hsr/gymtastic/technicalServices/utils/PdfExporter.java
+++ GymtasticApp/src/ch/hsr/gymtastic/technicalServices/utils/PdfExporter.java
@@ -16,7 +16,6 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
