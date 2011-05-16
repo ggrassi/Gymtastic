7c16e3eb4eccec56f788a4d0d3b7b3c090e0fbb0
Marco Pfiffner <m1pfiffn@hsr.ch>
2011-05-16 12:38:33 +0200

    nada

--- GymtasticApp/src/ch/hsr/gymtastic/technicalServices/network/RMIClient.java
+++ GymtasticApp/src/ch/hsr/gymtastic/technicalServices/network/RMIClient.java
@@ -12,60 +12,62 @@ import java.util.Observable;
 
 public class RMIClient extends Observable implements RMIClientInterface {
 
-    private String serverIP = "localhost";
-    private RMIServerInterface rmiServerInterface;
+	private String serverIP = "localhost";
+	private RMIServerInterface rmiServerInterface;
 
-    @Override
-    public void uploadSquadToClient(Serializable object) throws RemoteException {
-	updateObservers(object);
-    }
+	@Override
+	public void uploadSquadToClient(Serializable object) throws RemoteException {
+		updateObservers(object);
+	}
 
-    public RMIClient() throws Exception {
-    }
+	public RMIClient() throws Exception {
+	}
 
-    public RMIClient(String host) {
-	this.serverIP = host;
-    }
+	public RMIClient(String host) {
+		this.serverIP = host;
+	}
 
-    public RMIServerInterface connect(Serializable deviceType) throws RemoteException, NotBoundException,
-	    AccessException, ServerNotActiveException {
-	Registry registry = LocateRegistry.getRegistry(getServerIP());
-	rmiServerInterface = (RMIServerInterface) registry.lookup("Gymtastic");
-	RMIClientInterface stub = (RMIClientInterface) UnicastRemoteObject.exportObject(this, 0);
-	rmiServerInterface.addClient(stub, deviceType);
-	return rmiServerInterface;
+	public RMIServerInterface connect(Serializable deviceType)
+			throws RemoteException, NotBoundException, AccessException,
+			ServerNotActiveException {
+		Registry registry = LocateRegistry.getRegistry(getServerIP());
+		rmiServerInterface = (RMIServerInterface) registry.lookup("Gymtastic");
+		RMIClientInterface stub = (RMIClientInterface) UnicastRemoteObject
+				.exportObject(this, 0);
+		rmiServerInterface.addClient(stub, deviceType);
+		return rmiServerInterface;
 
-    }
+	}
 
-    public void disconnect() throws RemoteException {
-	rmiServerInterface.removeClient(this);
-    }
+	public void disconnect() throws RemoteException {
+		rmiServerInterface.removeClient(this);
+	}
 
-    public void setServerIP(String serverIP) {
-	this.serverIP = serverIP;
-    }
+	public void setServerIP(String serverIP) {
+		this.serverIP = serverIP;
+	}
 
-    public String getServerIP() {
-	return serverIP;
-    }
+	public String getServerIP() {
+		return serverIP;
+	}
 
-    public RMIServerInterface getRmiServerInterface() {
-	return rmiServerInterface;
-    }
+	public RMIServerInterface getRmiServerInterface() {
+		return rmiServerInterface;
+	}
 
-    private void updateObservers(Serializable object) {
-	setChanged();
-	notifyObservers(object);
-    }
+	private void updateObservers(Serializable object) {
+		setChanged();
+		notifyObservers(object);
+	}
 
-    @Override
-    public void uploadGymCupInfoToClient(Serializable object) {
-	updateObservers(object);
-    }
+	@Override
+	public void uploadGymCupInfoToClient(Serializable object) {
+		updateObservers(object);
+	}
 
-    @Override
-    public void uploadCompetitionInfoToClient(Serializable object) {
-	updateObservers(object);
-    }
+	@Override
+	public void uploadCompetitionInfoToClient(Serializable object) {
+		updateObservers(object);
+	}
 
 }
