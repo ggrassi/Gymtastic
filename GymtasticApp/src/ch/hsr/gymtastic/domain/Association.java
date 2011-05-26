f67b3e25b6ba2af5f43c3e4bc9a69c4b8b929821
Jules Weder <julesweder@gmail.com>
2011-05-26 17:16:52 +0200

    domain refactor

--- GymtasticApp/src/ch/hsr/gymtastic/domain/Association.java
+++ GymtasticApp/src/ch/hsr/gymtastic/domain/Association.java
@@ -12,101 +12,69 @@ import javax.persistence.Embeddable;
 public class Association implements Serializable {
 
     private static final long serialVersionUID = -3461109808214362308L;
-	private String name;
-	private String location;
+    private String name;
+    private String location;
 
-	/**
-	 * Instantiates a new association.
-	 *
-	 * @param name the name
-	 * @param location the location
-	 */
-	public Association(String name, String location) {
-		super();
-		this.name = name;
-		this.location = location;
-	}
+    /**
+     * Instantiates a new association.
+     * 
+     * @param name
+     *            the name
+     * @param location
+     *            the location
+     */
+    public Association(String name, String location) {
+	super();
+	this.name = name;
+	this.location = location;
+    }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-	    return name;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+	return name;
+    }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((location == null) ? 0 : location.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
+    /**
+     * Gets the name.
+     * 
+     * @return the name
+     */
+    public String getName() {
+	return name;
+    }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Association other = (Association) obj;
-		if (location == null) {
-			if (other.location != null)
-				return false;
-		} else if (!location.equals(other.location))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
+    /**
+     * Sets the name.
+     * 
+     * @param name
+     *            the new name
+     */
+    public void setName(String name) {
+	this.name = name;
+    }
 
-	/**
-	 * Gets the name.
-	 *
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * Gets the location.
+     * 
+     * @return the location
+     */
+    public String getLocation() {
+	return location;
+    }
 
-	/**
-	 * Sets the name.
-	 *
-	 * @param name the new name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/**
-	 * Gets the location.
-	 *
-	 * @return the location
-	 */
-	public String getLocation() {
-		return location;
-	}
-
-	/**
-	 * Sets the location.
-	 *
-	 * @param location the new location
-	 */
-	public void setLocation(String location) {
-		this.location = location;
-	}
+    /**
+     * Sets the location.
+     * 
+     * @param location
+     *            the new location
+     */
+    public void setLocation(String location) {
+	this.location = location;
+    }
 
 }
--- GymtasticApp/src/ch/hsr/gymtastic/domain/CompetitionInfo.java
+++ GymtasticApp/src/ch/hsr/gymtastic/domain/CompetitionInfo.java
@@ -6,35 +6,37 @@ import java.io.Serializable;
  * The Class CompetitionInfo holds the Name of the Competition
  */
 public class CompetitionInfo implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	private String competitionName;
 
-	/**
-	 * Instantiates a new competition info.
-	 *
-	 * @param name the name
-	 */
-	public CompetitionInfo(String name) {
-		this.competitionName = name;
-	}
+    private static final long serialVersionUID = 1L;
+    private String competitionName;
 
-	/**
-	 * Gets the competition name.
-	 *
-	 * @return the competition name
-	 */
-	public String getCompetitionName() {
-		return competitionName;
-	}
+    /**
+     * Instantiates a new competition info.
+     * 
+     * @param name
+     *            the name
+     */
+    public CompetitionInfo(String name) {
+	this.competitionName = name;
+    }
 
-	/**
-	 * Sets the competition.
-	 *
-	 * @param name the new competition
-	 */
-	public void setCompetition(String name) {
-		this.competitionName = name;
-	}
+    /**
+     * Gets the competition name.
+     * 
+     * @return the competition name
+     */
+    public String getCompetitionName() {
+	return competitionName;
+    }
+
+    /**
+     * Sets the competition.
+     * 
+     * @param name
+     *            the new competition
+     */
+    public void setCompetition(String name) {
+	this.competitionName = name;
+    }
 
 }
